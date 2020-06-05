4c4b543bebb97ac43c71b491955c5d191e31c6a9
mwinze2s <moritz.winzer@smail.inf.h-brs.de>
2020-06-05 10:37:43 +0200

    Profil zeigt jetzt aktuelle User Daten an Kleine Änderungen in diversen Klassen

--- src/main/java/org/HardCore/process/control/ProfileControl.java
+++ src/main/java/org/HardCore/process/control/ProfileControl.java
@@ -1,24 +1,12 @@
 package org.HardCore.process.control;
 
-import com.vaadin.ui.UI;
-import org.HardCore.gui.ui.MyUI;
-import org.HardCore.gui.windows.ConfirmationWindow;
-import org.HardCore.model.dao.RegisterDAO;
 import org.HardCore.model.dao.StudentDAO;
 import org.HardCore.model.dao.UnternehmenDAO;
 import org.HardCore.model.objects.dto.Student;
 import org.HardCore.model.objects.dto.Unternehmen;
 import org.HardCore.model.objects.dto.User;
 import org.HardCore.process.control.exceptions.DatabaseException;
-import org.HardCore.process.control.exceptions.EmailInUseException;
-import org.HardCore.process.control.exceptions.EmptyFieldException;
-import org.HardCore.process.control.exceptions.NoEqualPasswordException;
-import org.HardCore.services.db.JDBCConnection;
-import org.HardCore.services.util.Views;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.time.LocalDate;
 
 public class ProfileControl {
@@ -47,4 +35,9 @@ public class ProfileControl {
         unt_dao.setUnternehmenBranche(unternehmen,brancheValue);
 
     }
+    public static Student getStudent(User user){
+        return StudentDAO.getInstance().getAllDataStudent(user);
+    }
+
+    public static Unternehmen getUnternehmen(User user){ return UnternehmenDAO.getInstance().getAllDataUnternehmen(user);}
 }
