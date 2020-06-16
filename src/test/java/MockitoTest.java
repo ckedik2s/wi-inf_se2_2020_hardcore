0b0d58089b6103b0dbec3d58a99a3977b9f9f641
Adem <adem.efe@smail.inf.h-brs.de>
2020-06-16 02:00:36 +0200

    Testy

---
+++ src/test/java/MockitoTest.java
@@ -0,0 +1,18 @@
+import org.HardCore.process.control.LoginControl;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import static org.mockito.Mockito.*;
+
+public class MockitoTest {
+
+    @Mock
+    private LoginControl log;
+
+
+    @Test
+    public void testLogoutUser() {
+    }
+}
---
+++ src/test/java/UserDAOTest.java
@@ -0,0 +1,29 @@
+import org.HardCore.model.dao.UserDAO;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDAOTest {
+
+    private UserDAO userDAO;
+
+    @Test
+    void testCreate() {
+        userDAO =  UserDAO.getInstance();
+        assertNotNull(userDAO);
+    }
+    @Test
+    void testRead() {
+        assertNotNull(userDAO.getMaxID());
+    }
+    @Test
+    void testUpdate() {
+
+    }
+    @Test
+    void testDelete() {
+
+    }
+
+}
