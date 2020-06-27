7b11bfce7b6ea3fab72394aeba1e310aaaa95547
pwelte2s <patrick.welter@smail.inf.h-brs.de>
2020-06-27 20:49:16 +0200

    BewerbungDTO Factory clone und equals

--- src/main/java/org/HardCore/model/factory/BewerbungDTOFactory.java
+++ src/main/java/org/HardCore/model/factory/BewerbungDTOFactory.java
@@ -1,18 +1,23 @@
 package org.HardCore.model.factory;
 
 import org.HardCore.model.objects.dto.BewerbungDTO;
-import org.HardCore.model.objects.dto.StellenanzeigeDetail;
-import org.HardCore.model.objects.dto.UserDTO;
-import org.HardCore.model.objects.entities.Stellenanzeige;
-
-import java.sql.Date;
 
 public class BewerbungDTOFactory {
+
     public static BewerbungDTO createBewerbungDTO(int id, String text) {
         BewerbungDTO bewerbungDTO = new BewerbungDTO();
 
         bewerbungDTO.setId(id);
         bewerbungDTO.setFreitext(text);
         return bewerbungDTO;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    public Object clone() {
+        return null;
     }
 }
